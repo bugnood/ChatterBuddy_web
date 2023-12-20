577870ce692b03cb8e57d228804479d3f9abb362
daisuke.abe <commityneo@gmail.com>
2023-12-21 08:06:24 +0900

    初期資材をコミット

---
+++ src/main/java/com/ChatterBuddy/ChatterBuddy/ChatterBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ChatterBuddy.ChatterBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterBuddyApplication.class, args);
+	}
+
+}
